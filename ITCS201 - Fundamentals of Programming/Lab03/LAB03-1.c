52c34796ebbbd4eebb37f80f3053b7341e353988
Phumrapee Limpianchop <git@rayriffy.com>
2018-09-05 09:35:28 +0700

    :bug: Fixing text aligning issue

--- ITCS201 - Fundamentals of Programming/Lab03/LAB03-1.c
+++ ITCS201 - Fundamentals of Programming/Lab03/LAB03-1.c
@@ -2,6 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-  printf("Part No.\tPrice\nT1267\t\t\t$6.34\nT1300\t\t\t$8.92\nT2401\t\t\t$65.40\nT4482\t\t\t$36.99");
+  printf("%-10s %10s\n", "Part No.", "Price");
+  printf("%-10s %10s\n", "T1267", "$6.34");
+  printf("%-10s %10s\n", "T1300", "$8.92");
+  printf("%-10s %10s\n", "T2401", "$65.40");
+  printf("%-10s %10s", "T4482", "$36.99");
   return 0;
 }
