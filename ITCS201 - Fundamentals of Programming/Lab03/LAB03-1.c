2e8c578831130ceb3033c712fe91d4511147f097
Phumrapee Limpianchop <git@rayriffy.com>
2018-09-06 21:05:19 +0700

    :bug: Minor fix (?)

--- ITCS201 - Fundamentals of Programming/Lab03/LAB03-1.c
+++ ITCS201 - Fundamentals of Programming/Lab03/LAB03-1.c
@@ -2,10 +2,24 @@
 
 int main(int argc, char const *argv[])
 {
-  printf("%-10s %10s\n", "Part No.", "Price");
-  printf("%-10s %10s\n", "T1267", "$6.34");
-  printf("%-10s %10s\n", "T1300", "$8.92");
-  printf("%-10s %10s\n", "T2401", "$65.40");
-  printf("%-10s %10s", "T4482", "$36.99");
+  printf("Part No.    Price\n");
+  printf("T1267       $%.2f\n", 6.34);
+  printf("T1300       $%.2f\n", 8.92);
+  printf("T2401      $%.2f\n", 65.40);
+  printf("T4482      $%.2f", 36.99);
+
+  // This code is also works but without $
+  // printf("%-10s %10s\n", "Part No.", "Price");
+  // printf("%-10s %10.2f\n", "T1267", 6.34);
+  // printf("%-10s %10.2f\n", "T1300", 8.92);
+  // printf("%-10s %10.2f\n", "T2401", 65.40);
+  // printf("%-10s %10.2f", "T4482", 36.99);
+
+  // Original one that using string
+  // printf("%-10s %10s\n", "Part No.", "Price");
+  // printf("%-10s %10s\n", "T1267", "$6.34");
+  // printf("%-10s %10s\n", "T1300", "$8.92");
+  // printf("%-10s %10s\n", "T2401", "$65.40");
+  // printf("%-10s %10s", "T4482", "$36.99");
   return 0;
 }
--- ITCS201 - Fundamentals of Programming/Lab03/LAB03-B1.c
+++ ITCS201 - Fundamentals of Programming/Lab03/LAB03-B1.c
@@ -12,20 +12,33 @@ int main(int argc, char const *argv[])
   printf("Input the unit(mins) consumed by the customer:");
   scanf("%d", &b);
   
-  while (b != d) {
-    if (d < 100) {
-      e += 1;
-    }
-    else if (d >= 100 && d < 300) {
-      e += 0.75;
-    }
-    else if (d >= 300 && d < 600) {
-      e += 0.50;
-    }
-    else {
-      e += 0.25;
-    }
-    d++;
+  // while (b != d) {
+  //   if (d < 100) {
+  //     e += 1;
+  //   }
+  //   else if (d >= 100 && d < 300) {
+  //     e += 0.75;
+  //   }
+  //   else if (d >= 300 && d < 600) {
+  //     e += 0.50;
+  //   }
+  //   else {
+  //     e += 0.25;
+  //   }
+  //   d++;
+  // }
+
+  if (b >= 600) {
+    e = 400 + ((600 - b) * 0.25);
+  }
+  else if (b >= 300 && b < 600) {
+    e = 250 + ((b - 300) * 0.5);
+  }
+  else if (b >= 100 && b < 300) {
+    e = 100 + ((b - 100) * 0.75);
+  }
+  else {
+    e = b;
   }
 
   if (e > 300)
