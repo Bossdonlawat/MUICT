d34bf02636757327852ee5ba1783998afb367c1a
Phumrapee Limpianchop <git@rayriffy.com>
2018-09-07 13:18:11 +0700

    :art: Use tab instead of space

--- ITCS201 - Fundamentals of Programming/Lab03/LAB03-1.c
+++ ITCS201 - Fundamentals of Programming/Lab03/LAB03-1.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char const *argv[])
 {
-  printf("Part No.    Price\n");
-  printf("T1267       $%.2f\n", 6.34);
-  printf("T1300       $%.2f\n", 8.92);
-  printf("T2401      $%.2f\n", 65.40);
-  printf("T4482      $%.2f", 36.99);
+  printf("Part No.\t Price\n");
+  printf("T1267\t\t $%.2f\n", 6.34);
+  printf("T1300\t\t $%.2f\n", 8.92);
+  printf("T2401\t\t$%.2f\n", 65.40);
+  printf("T4482\t\t$%.2f", 36.99);
 
   // This code is also works but without $
   // printf("%-10s %10s\n", "Part No.", "Price");
