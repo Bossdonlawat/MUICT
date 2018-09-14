12c84f4ec114b80af049d09ebd7216cdbd205bfa
Phumrapee Limpianchop <git@rayriffy.com>
2018-09-14 11:47:51 +0700

    :art: Formatting

--- ITCS201 - Fundamentals of Programming/Lab05/LAB05-2.c
+++ ITCS201 - Fundamentals of Programming/Lab05/LAB05-2.c
@@ -4,9 +4,8 @@ int main(int argc, char const *argv[])
 {
   int i = 125;
   while (i >= 5) {
-    if (i % 25 != 0) {
+    if (i % 25 != 0)
       printf ("%d ", i);
-    }
     i -= 10;
   }
   return 0;
