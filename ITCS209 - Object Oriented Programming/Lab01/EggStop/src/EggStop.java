f74da38bf8d543ed5a4045edc28d19ad759d68c5
Phumrapee Limpianchop <git@rayriffy.com>
2019-01-14 00:54:00 +0700

    :sparkles: Lab 01

---
+++ ITCS209 - Object Oriented Programming/Lab01/EggStop/src/EggStop.java
@@ -0,0 +1,24 @@
+public class EggStop {
+  public static void main(String args[]) {
+    System.out.println("  ------");
+    System.out.println(" /      \\");
+    System.out.println("/        \\");
+    System.out.println("\\        /");
+    System.out.println(" \\      /");
+    System.out.println("  ------");
+    System.out.println("\n\n  ------");
+    System.out.println(" /      \\");
+    System.out.println("/        \\");
+    System.out.println("\\        /");
+    System.out.println(" \\      /");
+    System.out.println("  ------");
+    System.out.println("+--------+");
+    System.out.println("\n\n  ------");
+    System.out.println(" /      \\");
+    System.out.println("/        \\");
+    System.out.println("\\  STOP  |");
+    System.out.println("\\        /");
+    System.out.println(" \\      /");
+    System.out.println("  ------");
+  }
+}
